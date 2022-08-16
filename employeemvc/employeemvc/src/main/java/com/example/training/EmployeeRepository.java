af30fd39c7a4885f00c3e938fe9b3e54005309c7
echoudha <echoudha@ECHOUDHA-X40-J.us.oracle.com>
2022-08-16 14:50:21 +0530

    first commit message

---
+++ employeemvc/employeemvc/src/main/java/com/example/training/EmployeeRepository.java
@@ -0,0 +1,43 @@
+package com.example.training;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
+@Component //Creates object for EmployeeRepository
+public class EmployeeRepository {
+	private static List<Employee> database;
+	static {
+		database = new ArrayList();
+		database.add(new Employee(100, "James Cooper", 12345.6,"Trainer"));
+		database.add(new Employee(200, "Steven King", 12345.6,"Developer"));
+	}
+	
+	public Employee findEmployeeById(Integer id) {
+		System.out.println("EmployeeRepository.findEmployeeById: Parameter id is " + id);
+		for(Employee emp:database) {
+			if(emp.getId().equals(id))
+				return emp;
+		}
+		return null;
+	}
+
+	
+	public List<Employee> getAllEmployees(){
+		return database;
+	}
+	
+	public void createEmployee(Employee emp) {
+		database.add(emp);		
+	}
+	public void deleteEmployee(Integer empId) {
+		for(int i=0; i<database.size(); i++) {
+			if(database.get(i).getId().equals(empId))
+				database.remove(i);
+		}		
+	}
+
+	
+}
